0bee5eea2e828cda3aadcbb0c4fb3b55e10c95ad
Dzulqarnain Nasir <dzul@geta.no>
2016-08-09 17:45:31 +0300

    Add project files.

---
+++ EPi.Consuela.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPi.Consuela.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ EPi.Consuela/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EPi.Consuela.Global" Language="C#" %>
